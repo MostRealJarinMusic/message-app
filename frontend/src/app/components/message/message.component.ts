23a05aa15b8ef1438492ee6fd54fd156396e5ed5
MostRealJarinMusic <joshjarin2006@gmail.com>
2025-06-29 23:52:27 +0100

    Proper timestamps

--- frontend/src/app/components/message/message.component.ts
+++ frontend/src/app/components/message/message.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Message } from '@common/types';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
+import { isToday, isYesterday } from 'date-fns';
 
 @Component({
   selector: 'app-message',
@@ -13,11 +14,17 @@ export class MessageComponent {
   @Input() message!: Message;
 
   formatTime(timestamp: string): string {
-    const date = new Date(timestamp);
-    return date.toLocaleTimeString('en-US', {
+    const dateTime = new Date(timestamp);
+    const timeStr = dateTime.toLocaleTimeString('en-UK', {
       hour: 'numeric',
       minute: '2-digit',
-      hour12: true,
+      hour12: false,
     });
+
+    if (isToday(dateTime)) return timeStr;
+    if (isYesterday(dateTime)) return `Yesterday at ${timeStr}`;
+
+    const dateStr = dateTime.toLocaleDateString('en-UK');
+    return `${dateStr}, ${timeStr}`;
   }
 }
