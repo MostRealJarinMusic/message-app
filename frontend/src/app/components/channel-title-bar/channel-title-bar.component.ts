7539b3b6520a2122964b56d753a6d27cf376eaf1
MostRealJarinMusic <joshjarin2006@gmail.com>
2025-07-17 23:44:52 +0100

    Feature/grid based dashboard styling

--- frontend/src/app/components/channel-title-bar/channel-title-bar.component.ts
+++ frontend/src/app/components/channel-title-bar/channel-title-bar.component.ts
@@ -1,9 +1,13 @@
 import { Component, effect, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { ChannelService } from 'src/app/services/channel/channel.service';
+import { InputIcon } from 'primeng/inputicon';
+import { IconField } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-channel-title-bar',
-  imports: [],
+  imports: [ButtonModule, InputIcon, IconField, InputTextModule],
   templateUrl: './channel-title-bar.component.html',
   styleUrl: './channel-title-bar.component.scss',
 })
