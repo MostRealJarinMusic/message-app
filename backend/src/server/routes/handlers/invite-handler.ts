01aeadbf660ce07d160a19cb9a72d723c0c74019
MostRealJarinMusic <joshjarin2006@gmail.com>
2025-09-04 21:40:06 +0100

    Invite handler rework

--- backend/src/server/routes/handlers/invite-handler.ts
+++ backend/src/server/routes/handlers/invite-handler.ts
@@ -1,5 +1,3 @@
-import { Response } from "express";
-import { SignedRequest } from "../../../types/types";
 import {
   ServerInvite,
   ServerInviteCreate,
@@ -12,138 +10,100 @@ import { ServerInviteRepo } from "../../../db/repos/server-invite.repo";
 import { ServerMemberRepo } from "../../../db/repos/server-member.repo";
 import { WebSocketManager } from "../../../server/ws/websocket-manager";
 import { ServerRepo } from "../../../db/repos/server.repo";
+import {
+  BadRequestError,
+  ConflictError,
+  NotFoundError,
+} from "../../../errors/errors";
 
 export class InviteHandler {
   //Create invite
-  static async createInvite(req: SignedRequest, res: Response) {
-    try {
-      const newInviteData = req.body as ServerInviteCreate;
+  static async createInvite(inviteCreate: ServerInviteCreate) {
+    if (!inviteCreate) throw new BadRequestError("Invite data required");
 
-      if (!newInviteData) {
-        res.status(400).json({ error: "Server data required" });
-        return;
-      }
+    //Check permissions
 
-      //Check permissions
+    const createdAt = new Date();
 
-      const createdAt = new Date();
+    const invite: ServerInvite = {
+      id: ulid(),
+      serverId: inviteCreate.serverId,
+      link: uuid(),
+      createdAt: createdAt.toISOString(),
+      expiresOn: inviteCreate.expiresOn ?? new Date().toISOString(),
+    };
 
-      const invite: ServerInvite = {
-        id: ulid(),
-        serverId: newInviteData.serverId,
-        link: uuid(),
-        createdAt: createdAt.toISOString(),
-        expiresOn: newInviteData.expiresOn ?? new Date().toISOString(),
-      };
+    await ServerInviteRepo.createServerInvite(invite);
 
-      await ServerInviteRepo.createServerInvite(invite);
-
-      res.status(204).send();
-    } catch (err) {
-      res.status(500).json({ error: "Failed to create invite" });
-    }
+    return invite;
   }
 
   //Get invite (preview)
-  static async previewInvite(req: SignedRequest, res: Response) {
-    try {
-      const inviteId = req.params.inviteId;
-      const invite = await ServerInviteRepo.getServerInvite(inviteId);
+  static async previewInvite(inviteId: string) {
+    const invite = await ServerInviteRepo.getServerInvite(inviteId);
+    if (!invite) throw new NotFoundError("Invite doesnt exist");
 
-      if (!invite) {
-        res.status(404).json({ error: "Invite doesn't exist" });
-        return;
-      }
-
-      res.json(invite);
-    } catch (err) {
-      res.status(500).json({ error: "Failed to get invite" });
-    }
+    return invite;
   }
 
   //Accept invite
   static async acceptInvite(
-    req: SignedRequest,
-    res: Response,
+    inviteId: string,
+    userId: string,
     wsManager: WebSocketManager
   ) {
-    try {
-      const inviteId = req.params.inviteId;
-      const invite = await ServerInviteRepo.getServerInvite(inviteId);
+    const invite = await ServerInviteRepo.getServerInvite(inviteId);
 
-      if (!invite || !invite.serverId) {
-        res.status(404).json({ error: "Invite doesn't exist" });
-        return;
-      }
+    if (!invite || !invite.serverId)
+      throw new NotFoundError("Invite doesn't exist");
 
-      //Check if it is expired
+    //Check if it is expired
 
-      //Check if already part of server
-      const userId = req.signature!.id;
+    //Check if already part of server
+    const existingMember = await ServerMemberRepo.getServerMember(
+      invite.serverId,
+      userId
+    );
 
-      const existingMember = await ServerMemberRepo.getServerMember(
-        invite.serverId,
-        userId
-      );
+    if (existingMember)
+      throw new BadRequestError("User is already a member of server");
 
-      if (existingMember) {
-        res.status(400).json({ error: "User already part of server" });
-        return;
-      }
+    //Accept
+    const server = await ServerRepo.getServer(invite.serverId);
+    //Add server member
+    const member: ServerMember = {
+      userId: userId,
+      serverId: server.id,
+    };
+    await ServerMemberRepo.addServerMember(member);
 
-      //Accept
-      const server = await ServerRepo.getServer(invite.serverId);
-      //Add server member
-      const member: ServerMember = {
-        userId: userId,
-        serverId: server.id,
-      };
-      await ServerMemberRepo.addServerMember(member);
+    //WebSocket message to notify other server members
+    const memberIds = await ServerMemberRepo.getServerMemberIds(server.id);
+    wsManager.broadcastToGroup(
+      WSEventType.SERVER_MEMBER_ADD,
+      member,
+      memberIds.filter((m) => m !== userId)
+    );
 
-      //WebSocket message to notify other server members
-      const memberIds = await ServerMemberRepo.getServerMemberIds(server.id);
-      wsManager.broadcastToGroup(
-        WSEventType.SERVER_MEMBER_ADD,
-        member,
-        memberIds.filter((m) => m !== userId)
-      );
-
-      //Return server
-      res.json(server);
-    } catch (err) {
-      res.status(500).json({ error: "Failed to accept invite" });
-    }
+    //Return server
+    return server;
   }
 
   //Revoke invite
-  static async revokeInvite(req: SignedRequest, res: Response) {
-    try {
-      const inviteId = req.params.inviteId;
-      const invite = await ServerInviteRepo.getServerInvite(inviteId);
+  static async revokeInvite(inviteId: string) {
+    const invite = await ServerInviteRepo.getServerInvite(inviteId);
 
-      if (!invite || !invite.serverId) {
-        res.status(404).json({ error: "Invite doesn't exist" });
-        return;
-      }
+    if (!invite || !invite.serverId)
+      throw new NotFoundError("Invite doesn't exist");
 
-      //Check permissions here
+    //Check permissions here
 
-      await ServerInviteRepo.deleteServerInvite(inviteId);
-      res.status(204).send();
-    } catch (err) {
-      res.status(500).json({ error: "Failed to delete invite" });
-    }
+    await ServerInviteRepo.deleteServerInvite(inviteId);
   }
 
   //Get server invites
-  static async getServerInvites(req: SignedRequest, res: Response) {
-    try {
-      const serverId = req.params.serverId;
-      const invites = await ServerInviteRepo.getServerInvites(serverId);
-
-      res.json(invites);
-    } catch (err) {
-      res.status(500).json({ error: "Failed to load server invites" });
-    }
+  static async getServerInvites(serverId: string) {
+    const invites = await ServerInviteRepo.getServerInvites(serverId);
+    return invites;
   }
 }
--- backend/src/server/routes/invite-routes.ts
+++ backend/src/server/routes/invite-routes.ts
@@ -3,33 +3,49 @@ import { WebSocketManager } from "../ws/websocket-manager";
 import { authMiddleware } from "../..//middleware/auth-middleware";
 import { InviteHandler } from "./handlers/invite-handler";
 import { SignedRequest } from "../../types/types";
+import { asyncHandler } from "../../utils/async-wrapper";
 
 export default function inviteRoutes(wsManager: WebSocketManager): Router {
   const inviteRoutes = Router();
 
-  inviteRoutes.post("", authMiddleware, async (req: Request, res: Response) => {
-    InviteHandler.createInvite(req as SignedRequest, res);
-  });
+  inviteRoutes.post(
+    "",
+    authMiddleware,
+    asyncHandler(async (req: Request, res: Response) => {
+      const invite = await InviteHandler.createInvite(req.body);
+      res.status(201).json(invite);
+    })
+  );
 
   inviteRoutes.get(
     "/:inviteId",
     authMiddleware,
-    async (req: Request, res: Response) => {
-      InviteHandler.previewInvite(req as SignedRequest, res);
-    }
+    asyncHandler(async (req: Request, res: Response) => {
+      const invite = await InviteHandler.previewInvite(req.params.inviteId);
+      res.json(invite);
+    })
   );
 
   inviteRoutes.post(
     "/:inviteId/accept",
     authMiddleware,
-    async (req: Request, res: Response) => {
-      InviteHandler.acceptInvite(req as SignedRequest, res, wsManager);
-    }
+    asyncHandler(async (req: SignedRequest, res: Response) => {
+      const server = InviteHandler.acceptInvite(
+        req.params.inviteId,
+        req.signature!.id,
+        wsManager
+      );
+      res.json(server);
+    })
   );
 
-  inviteRoutes.delete("/:inviteId", async (req: Request, res: Response) => {
-    InviteHandler.revokeInvite(req as SignedRequest, res);
-  });
+  inviteRoutes.delete(
+    "/:inviteId",
+    asyncHandler(async (req: Request, res: Response) => {
+      await InviteHandler.revokeInvite(req.params.inviteId);
+      res.status(204).send();
+    })
+  );
 
   return inviteRoutes;
 }
