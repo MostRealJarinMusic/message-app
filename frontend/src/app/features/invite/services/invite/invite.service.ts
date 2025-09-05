4269f4e469c1087c3a3d7b26bc9c8b4696a8e688
MostRealJarinMusic <joshjarin2006@gmail.com>
2025-09-05 21:51:03 +0100

    Invite service

--- frontend/src/app/features/invite/services/invite/invite.service.ts
+++ frontend/src/app/features/invite/services/invite/invite.service.ts
@@ -1,10 +1,33 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { LoggerType, ServerInviteCreate } from '@common/types';
+import { PrivateApiService } from 'src/app/core/services/api/private-api.service';
+import { LoggerService } from 'src/app/core/services/logger/logger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InviteService {
+  private apiService = inject(PrivateApiService);
+  private logger = inject(LoggerService);
+
   constructor() {}
 
-  previewInvite(inviteId: string) {}
+  public previewInvite(inviteLink: string) {
+    this.apiService.previewInvite(inviteLink).subscribe({
+      next: (invite) => invite,
+      error: (err) => this.logger.error(LoggerType.SERVICE_INVITE, 'Failed to preview invite', err),
+    });
+  }
+
+  public createInvite(serverId: string) {
+    const inviteCreate: ServerInviteCreate = {
+      serverId,
+    };
+
+    return this.apiService.createInvite(inviteCreate);
+  }
+
+  public acceptInvite(inviteLink: string) {
+    return this.apiService.acceptInvite(inviteLink);
+  }
 }
