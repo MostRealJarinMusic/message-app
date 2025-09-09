d090616b5b615bfc0eec1eaaed6907507b6bc09f
MostRealJarinMusic <joshjarin2006@gmail.com>
2025-09-09 22:02:24 +0100

    User panel formatting

--- frontend/src/app/features/user/components/user-panel/user-panel.component.ts
+++ frontend/src/app/features/user/components/user-panel/user-panel.component.ts
@@ -1,15 +1,20 @@
 import { Component, effect, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { UserService } from '../../services/user/user.service';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { AvatarModule } from 'primeng/avatar';
+import { PresenceService } from '../../services/presence/presence.service';
 
 @Component({
   selector: 'app-user-panel',
-  imports: [CardModule],
+  imports: [CardModule, ButtonModule, CommonModule, AvatarModule],
   templateUrl: './user-panel.component.html',
   styleUrl: './user-panel.component.scss',
 })
 export class UserPanelComponent {
   private userService = inject(UserService);
+  protected presenceService = inject(PresenceService);
 
   protected username = '';
 
